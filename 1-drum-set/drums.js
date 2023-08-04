3ccea5f416aee2cb79ee2ed8ecbf2ad73c3a9ab5
georgehetrelezis <georgehetrelezis@gmail.com>
2023-08-04 14:14:52 +1000

    [Code] Connecting all buttons + CSS changes.

--- 1-drum-set/drums.js
+++ 1-drum-set/drums.js
@@ -1,1 +1,19 @@
 
+let updatePage = function(event) {
+    if(event.propertyName !== 'transform') return;
+    this.classList.remove('activated');
+}
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(key => key.addEventListener('transitionend', updatePage))
+
+let activateAudio = function(event) {
+    const sound = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const button = document.querySelector(`.button[data-key="${event.keyCode}"]`);
+    if(!sound) return;
+    sound.begin=0;
+    sound.play();
+    button.classList.add('activated');
+}
+  
+window.addEventListener('keydown',activateAudio);
